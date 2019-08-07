dfcd3b98967082ccecfec0334216419b8c5532f6
badwulf51 <aronomalley@yahoo.com>
2019-08-07 20:36:46 +0100

    finished off part 1 and 2 of homework, doing final part now

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -13,26 +13,43 @@ def sum(array)
 	end
 end
 
-def max_2_sum arr
-  # YOUR CODE HERE
+def max_2_sum(array)
+	max1 = 0
+	max2 = 0 
+	total = 0
+	if array.length > 1
+		max1 = array.sort[-1]
+		max2 = array.sort[-2]
+		total = max1 + max2 
+		return total
+		else if array.length == 1
+			return array[0]
+			else
+				return 0
+		end 
+	end
 end
 
-def sum_to_n? arr, n
+def sum_to_n? (arr, n)
   # YOUR CODE HERE
+ return arr.combination(2).any? {|a, b| a + b == n }
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+	"Hello, #{name}"
 end
 
-def starts_with_consonant? s
+def starts_with_consonant? (s)
   # YOUR CODE HERE
+  return /^[b-df-hj-np-tv-z]/i.match(s) != nil
+  
 end
 
-def binary_multiple_of_4? s
-  # YOUR CODE HERE
+def binary_multiple_of_4? (s)
+	return true if s == "0"
+	/^[10]*00$/.match(s) != nil
 end
 
 # Part 3
